5b6b1d5b484b57accf790f208a23fd9ddf9320bb
zhangluzheng <1152165014@qq.com>
2021-03-22 00:26:55 +0800

    新

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ import json
 #开始下载 1164 641
 #后台下载 1185 690
 #取样点 1127 637
-cookie_str="password=b32ce4aaef307876b68c38a7e1024615; __cfduid=d192b1f4e5a45c989ab4172e838c25fe51610516206; cf_chl_prog=a23; cf_clearance=7b298fe486bb823d4218530a43fdab3ed7f73dce-1611047389-0-250; timezone=8; __cf_bm=5133d2742aa97ab2d4155ccce3caf424066dd714-1611047392-1800-ARqqsXo525cVSQhjEZwXQhdee5+3amBgEvmwVWb6graCKzQlDxJzYA+fuW+ojY10oE0j/+bRMBOTMDx90Y6p3c/UgIHExh5YaD9ryCja7VFXN0OLTJWEskAUFySB+yqI+g=="
+proxy={
+'http':'127.0.0.1:10809',
+'https':'127.0.0.1:10809'
+}
+cookie_str="__cfduid=dc8cef069fd08bc183e2917ed9b54646f1616307611; darkmode=false; __cf_bm=81ca7052dd2d98beb16909dfa7ab52c5eed5f8f7-1616343185-1800-AT6UJ7nk0bXmnRz61aekvdVNrc74xuLsPgX8QCui9+gp7SXWoTIBDgsGCXLE+K+PxRZ1gFn7HJl8a6O/b2HaIBS1XcPOECKW7i6r8Sq6lIt0YguL/uHrpEP+TTrSFJeRyw=="
 def make_cookie(cookie_str):
     cookie_json={}
     cookies=cookie_str.split(";")
@@ -24,18 +28,19 @@ json_data={
 page_header={
     "authority": "sj-ghs.club",
     "method": "GET",
-    "path": "/2020/1000/booth/booth/(SWDPN0005)%201148959.7z",
+    "path": "/2020/1106/getchu/",
     "scheme": "https",
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "accept-language": "zh-CN,zh;q=0.9",
     "cache-control": "no-cache",
     "pragma": "no-cache",
+    "referer": "https://sj-ghs.club/2020/1106/",
     "sec-fetch-dest": "document",
     "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
+    "sec-fetch-site": "same-origin",
     "sec-fetch-user": "?1",
     "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
 }
 down_header={
     "authority": "sj-ghs.club",
@@ -52,7 +57,7 @@ down_header={
     "sec-fetch-site": "same-origin",
     "sec-fetch-user": "?1",
     "upgrade-insecure-requests": "1",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
 }   
 def click_down():
     pyautogui.moveTo(1164,641,duration=1,tween=pyautogui.linear)
@@ -65,10 +70,10 @@ def click_down():
     else:
         return
 def get_txt():
-    url="https://sj-ghs.club/2020/1000/booth/booth"
+    url="https://sj-ghs.club/2021/0120/getchu/"
     #url="https://dl.smileacg.com/CN3/sf19/"
     #main_page=requests.get(url,cookies=cookie)
-    main_page=requests.get(url,cookies=cookie,headers=page_header)
+    main_page=requests.get(url,cookies=cookie,headers=page_header,proxies=proxy)
     rc=re.compile(r'/\d+/[a-z\;\&]+/.*?\.[7ztx]+\.?\d*')
     #rc=re.compile(r'[^<>"]+?.7z')
     items=rc.findall(main_page.text)
@@ -85,25 +90,54 @@ def get_link():
     f=open("./1.txt")
     for line in f.readlines():
         down_url=line.strip("\n")
-        page=requests.get("https://sj-ghs.club/2020"+down_url,cookies=cookie,allow_redirects=False)
+        page=requests.get("https://sj-ghs.club/2021"+down_url,cookies=cookie,allow_redirects=False,proxies=proxy)
         t_down_url=page.next.url
         f=open("./2.txt","a")
         f.write(t_down_url+"\n")
         print(line)
 def downfromtxt():
+    i=0
     f=open("./1.txt","r")
     down_urls=[]
     for line in f.readlines():
         down_urls.append(line.strip("\n"))
+    while(True):
+        try:
+            if(i>=len(down_urls)):
+                break
+            down_url=down_urls[i]
+            down_url=down_url.strip("\n")
+            page=requests.get("https://sj-ghs.club/2021"+down_url,cookies=cookie,headers=down_header,allow_redirects=False)
+            #page=requests.get("https://dl.smileacg.com/"+down_url,cookies=cookie,allow_redirects=False)
+            t_down_url=page.next.url
+            pyperclip.copy(t_down_url)
+            page.close()
+            pyautogui.moveTo(625,70,duration=1,tween=pyautogui.linear)
+            pyautogui.click(x=625, y=70, clicks=1, interval=0.01, button='left', duration=0.1, tween=pyautogui.linear) 
+            pyautogui.moveTo(839,482,duration=1,tween=pyautogui.linear)
+            pyautogui.click(x=839,y=482,clicks=1,interval=0,button='left',duration=0.1,tween=pyautogui.linear)
+            #pyautogui.moveTo(800,400,duration=1,tween=pyautogui.linear)
+            #pyautogui.typewrite(message=t_down_url,interval=0.001)
+            pyautogui.hotkey('ctrl', 'v')
+
+            click_down()
+            time.sleep(5)
+
+            pyautogui.moveTo(1185,690,duration=1,tween=pyautogui.linear)
+            pyautogui.click(x=1185,y=690,clicks=1,interval=0,button='left',duration=0.1,tween=pyautogui.linear)
+            i=i+1
+        except:
+            continue
+"""
     for down_url in down_urls:
         down_url=down_url.strip("\n")
-        page=requests.get("https://sj-ghs.club/2020"+down_url,cookies=cookie,headers=down_header,allow_redirects=False)
+        page=requests.get("https://sj-ghs.club/2021"+down_url,cookies=cookie,headers=down_header,allow_redirects=False)
         #page=requests.get("https://dl.smileacg.com/"+down_url,cookies=cookie,allow_redirects=False)
         t_down_url=page.next.url
         pyperclip.copy(t_down_url)
         page.close()
-        pyautogui.moveTo(678,72,duration=1,tween=pyautogui.linear)
-        pyautogui.click(x=678, y=72, clicks=1, interval=0.01, button='left', duration=0.1, tween=pyautogui.linear) 
+        pyautogui.moveTo(625,70,duration=1,tween=pyautogui.linear)
+        pyautogui.click(x=625, y=70, clicks=1, interval=0.01, button='left', duration=0.1, tween=pyautogui.linear) 
         pyautogui.moveTo(839,482,duration=1,tween=pyautogui.linear)
         pyautogui.click(x=839,y=482,clicks=1,interval=0,button='left',duration=0.1,tween=pyautogui.linear)
         #pyautogui.moveTo(800,400,duration=1,tween=pyautogui.linear)
@@ -115,6 +149,7 @@ def downfromtxt():
 
         pyautogui.moveTo(1185,690,duration=1,tween=pyautogui.linear)
         pyautogui.click(x=1185,y=690,clicks=1,interval=0,button='left',duration=0.1,tween=pyautogui.linear)
+"""
 def downfromlink():
     f=open("./2.txt")
     for line in f.readlines():
